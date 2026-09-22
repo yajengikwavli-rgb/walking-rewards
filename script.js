2ec50f416479e547ce8993f870c2c6153ea3d942
yajengikwavli-rgb <yajengikwavli@gmail.com>
2026-09-22 08:18:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -22,12 +22,12 @@ function updateDisplay() {
     document.getElementById("steps").textContent = steps;
     document.getElementById("points").textContent = points;
 
-    let progress = steps % 8,000;
-    let percentage = (progress / 8,000) * 100;
+    let progress = steps % 8000;
+    let percentage = (progress / 8000) * 100;
 
     document.getElementById("progress").style.width = percentage + "%";
     document.getElementById("progressText").textContent =
-        progress + " / 8,000 steps";
+        progress + " / 8000 steps";
 
     displayRewards();
     displayHistory();
